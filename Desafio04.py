ce14741de4ad6c93583c6264e6fcaa50c44bee2c
Taissa Lima <taissasli@gmail.com>
2022-07-08 02:25:05 -0300

    Desafio04

---
+++ Desafio04.py
@@ -0,0 +1,4 @@
+n = str(input("Digite uma palavra ou frase"))
+print(n[::-1])
+#Isso acontece porque o comando em vez de pular de um em um, ele decresce de um em um e imprime.
+#Metodo: Fatiamento de string.
